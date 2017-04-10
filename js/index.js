0b16eeec7441f36c791fe7645057cce180785ca9
Andrey Mordasov <baskin-robins@ya.ru>
2017-04-10 16:09:03 +0300

    Square component is now stateless.

--- js/index.js
+++ js/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
